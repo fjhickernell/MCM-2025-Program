6662ef025fcd4d704029b46eca33e3ab07610314
Sou-Cheng Choi <schoi32@iit.edu>
2025-07-01 20:07:03 -0500

    Fix rotated schedule pages

--- build_programbook.sh
+++ build_programbook.sh
@@ -39,11 +39,11 @@ mv MCM2025_book.pdf "MCM2025_Book${suffix}.pdf"
 
 # Generate schedules
 /opt/homebrew/bin/pdftk MCM2025_book${suffix}.pdf cat 25-26 output MCM2025_schedule1sheet${suffix}.pdf
-/opt/homebrew/bin/pdftk MCM2025_book${suffix}.pdf cat 24-27 28-36east output MCM2025_schedule${suffix}.pdf
-/opt/homebrew/bin/pdftk MCM2025_book${suffix}.pdf cat 24-27 28-36east 37-227 output MCM2025_schedule_abstracts${suffix}.pdf
+/opt/homebrew/bin/pdftk MCM2025_book${suffix}.pdf cat 24-26 27-35east output MCM2025_schedule${suffix}.pdf
+/opt/homebrew/bin/pdftk MCM2025_book${suffix}.pdf cat 24-26 27-35east 36-227 output MCM2025_schedule_abstracts${suffix}.pdf
 
 # Rotate pages 12-20 clockwise 90 degrees
-/opt/homebrew/bin/pdftk "MCM2025_Book${suffix}.pdf" cat 1-27 28-36east 37-end output "MCM2025_Book${suffix}_rotated.pdf"
+/opt/homebrew/bin/pdftk "MCM2025_Book${suffix}.pdf" cat 1-26 27-35east 36-end output "MCM2025_Book${suffix}_rotated.pdf"
 mv "MCM2025_Book${suffix}_rotated.pdf" "MCM2025_Book${suffix}.pdf"
 
 # Open PDFs
