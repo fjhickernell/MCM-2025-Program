0cad0e47dfd6b0cb06d7ef5fe602882c406d27e3
Fred Hickernell <fjhickernell.IIT@gmail.com>
2025-07-13 19:48:18 -0500

    rerun program book

--- build_programbook.sh
+++ build_programbook.sh
@@ -26,10 +26,10 @@ TEXBIN=/usr/local/texlive/${TEXYEAR}/bin/universal-darwin
 # page numbers for important sections, these need to be updated manually if the document changes
 # These numbers are based on the current structure of the MCM program book.
 # If you add or remove pages, you will need to adjust these numbers accordingly.
-SCHED_START=26 #where the schedule cover page starts
-OUTLINE_END=28  #where the outline schedule ends
-SCHED_END=37  #where the schedule ends
-ABSTRACT_END=228  #where the abstracts end
+SCHED_START=28 #where the schedule cover page starts
+OUTLINE_END=30  #where the outline schedule ends
+SCHED_END=39  #where the schedule ends
+ABSTRACT_END=231  #where the abstracts end
 
 echo "Using filename suffix: $suffix"
 
